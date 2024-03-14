1ef4684940d5cc2c8911fe7f0826b36bd6b4c296
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-03-14 13:58:28 +0530

    Add files via upload

---
+++ variables_5.py
@@ -0,0 +1,20 @@
+# int
+a1 = 100
+print(a1)
+print(type(a1))
+
+# float
+b2 = 2.100
+print(b2)
+print(type(b2))
+
+# boolen
+c3 = True
+print(c3)
+print(type(c3))
+
+# string
+d4 = "yogeesh"
+print(d4)
+print(type(d4))
+
